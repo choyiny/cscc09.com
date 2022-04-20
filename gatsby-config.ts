f40ddf372da1e35d05539e49b2c9a85fa64fc2f0
Cho Yin Yong <choyiny@gmail.com>
2022-04-20 19:31:41 -0400

    feat: add google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -62,6 +62,27 @@ const config: GatsbyConfig = {
         isUsingColorMode: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-GECBSGY4LR",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "cscc09.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ]
 };
 
