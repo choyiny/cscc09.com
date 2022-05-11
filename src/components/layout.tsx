0a0a078b0d4ac16fe6f9c27c5b97ebcc797d39cb
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2022-05-11 17:41:49 -0400

    feat(chakra): use system color as default

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,10 +3,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Navigation from "./navigation";
 import Footer from "./footer";
 import Seo from "./seo";
+import theme from "../constants/theme";
 
 export default function Layout({ children, activePage }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Seo />
       <Navigation />
       <main>{children}</main>
