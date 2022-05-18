b62575c5844116a03f6824eb72f3f5e8a693105d
Cho Yin Yong <choyiny@gmail.com>
2022-05-17 23:12:11 -0400

    chore: changes from lecture

--- lectures/javascript/basic/js/script.js
+++ lectures/javascript/basic/js/script.js
@@ -1,17 +1,24 @@
-function createParagraph(content) {
-  const elmnt = document.createElement("p");
-  elmnt.innerHTML = content;
-  return elmnt;
-}
+(function() {
+  "use strict";
 
-window.onload = function () {
-  document.querySelector(
-    ".content"
-  ).innerHTML = `<h1 class="title">This is a heading.</h1>`;
+  function createParagraph(content) {
+    // <p></p>
+    const elmnt = document.createElement("p");
+    // <p>{ content }</p>
+    elmnt.innerHTML = content;
+    return elmnt;
+  }
 
-  document.querySelector("#generate-paragraph").onclick = function() {
-    document
-      .querySelector(".content")
-      .appendChild(createParagraph("This is a paragraph."));
+  window.onload = function () {
+    document.querySelector(
+      ".content"
+    ).innerHTML = `<h1 class="title">This is a heading.</h1>`;
+
+    document.querySelector("#generate-paragraph").onclick = function() {
+      document
+        .querySelector(".content")
+        .appendChild(createParagraph("This is a paragraph."));
+    };
   };
-};
+
+}());
