37167f58c9a21a3a4911748e2aadd4e8c9faaa18
Cho Yin Yong <choyiny@gmail.com>
2022-05-24 01:05:32 -0400

    chore: more lecture 3 material

---
+++ lectures/03-api/basic/js/script.js
@@ -0,0 +1,46 @@
+(function () {
+  "use strict";
+
+  function send(method, url, data, callback) {
+    const config = {
+      method: method,
+    };
+    if (!["GET", "DELETE"].includes(method)) {
+      config["headers"] = {
+        "Content-Type": "application/json",
+      };
+      config["body"] = JSON.stringify(data);
+    }
+    fetch(url, config)
+      .then((res) => res.json())
+      .then((val) => callback(null, val));
+  }
+
+  function createParagraph(content) {
+    // <p></p>
+    const elmnt = document.createElement("p");
+    // <p>{ content }</p>
+    elmnt.innerHTML = content;
+    return elmnt;
+  }
+
+  function generateJoke(callback) {
+    send(
+      "GET",
+      "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist,explicit",
+      {},
+      callback
+    );
+  }
+
+  window.onload = function () {
+    // onclick listener for generating a paragraph
+    document.querySelector("#generate-paragraph").onclick = function () {
+      generateJoke((_, data) => {
+        document
+          .querySelector(".content")
+          .appendChild(createParagraph(data.joke));
+      });
+    };
+  };
+})();
